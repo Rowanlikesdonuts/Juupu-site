81be45b264ea6e8f3025ddc64456e025d90ae0c5
Rowanlikesdonuts <74143814+Rowanlikesdonuts@users.noreply.github.com>
2024-09-03 15:21:32 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -33,3 +33,13 @@
         link.addEventListener('click', handleLinkClick);
     });
 
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
