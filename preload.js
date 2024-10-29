a3a5a608eb9e1e02ad82913ada89683c88aef49c
Rowanlikesdonuts <74143814+Rowanlikesdonuts@users.noreply.github.com>
2024-10-29 14:21:59 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+<script src="https://kit.fontawesome.com/4600eb7943.js" crossorigin="anonymous"></script>
+
 // Dit is een script dat alle pagina's van de site van te voren downloadt op de achtergrond. Dat zorgt voor een snellere site.
 
     // Function to handle link clicks
